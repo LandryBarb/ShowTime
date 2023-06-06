f6be4cc0c2108ef610e6e8921f53beda3029f404
Landry <landry@landrybarb.com>
2023-06-05 17:32:39 -0700

    update of home page- animations and image

--- wwwroot/js/parallax.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("scroll", function() {
-    const parallax = document.querySelector("#pitch");
-    let scrollPosition = window.pageYOffset;
-
-    parallax.style.transform = "translateY(" + scrollPosition * 0.5 + "px)";
-});
